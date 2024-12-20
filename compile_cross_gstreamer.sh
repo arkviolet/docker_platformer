f3e84a46d191e27432ff4d6e4422c4248345f49e
houtianxin <houtianxin@makeblock.com>
2024-12-20 14:23:22 +0800

    add gstreamer

---
+++ compile_cross_gstreamer.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -ex
+
+sourcePath=/home/developer/workspace
+
+PACKAGE_COMPILE_DIR=$sourcePath/packages
+INSTALL_PREFIX_CROSS_LINUX=$sourcePath/software/cross
+
+cd $sourcePath
+
+mkdir -vp $PACKAGE_COMPILE_DIR
+mkdir -vp $INSTALL_PREFIX_CROSS_LINUX
+
+##########################################################################################
+# rk3568
+cd $PACKAGE_COMPILE_DIR
+
+git clone -b 1.20 --depth=1 https://gitlab.freedesktop.org/gstreamer/gstreamer.git
+
+cd gstreamer && cp $sourcePath/docker_platformer/patches/gst_rk3568.txt .
+
+meson --cross-file gst_rk3568.txt build_rk3568 \
+    -Dgtk_doc=disabled \
+    -Dtests=disabled \
+    -Dexamples=disabled \
+    --reconfigure \
+    --prefix=$INSTALL_PREFIX_CROSS_LINUX/gstreamer/rk3568
+
+ninja -C build_rk3568
+
+meson install -C build_rk3568/
+
+ninja -C build_rk3568 clean
+
+##########################################################################################
---
+++ compile_cross_gun.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -ex
+
+sourcePath=/home/developer/workspace
+
+PACKAGE_COMPILE_DIR=$sourcePath/packages
+INSTALL_PREFIX_CROSS_LINUX=$sourcePath/software/cross
+
+cd $sourcePath
+
+mkdir -vp $PACKAGE_COMPILE_DIR
+mkdir -vp $INSTALL_PREFIX_CROSS_LINUX
+
+##########################################################################################
+# gcc
+
+# gmp
+
+# mpfr
+
+# mpc
+
+# gdb
+
+# binutils
